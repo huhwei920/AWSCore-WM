4ca958ad7d1c9690ef85955ffe0ebbabac8e44eb
huhangwei <huhangwei@gmail.com>
2024-07-26 18:51:29 +0800

    2.36.8

--- AWSCore-WM/AWSCore/AWSS3/AWSS3TransferUtilityDatabaseHelper.m
+++ AWSCore-WM/AWSCore/AWSS3/AWSS3TransferUtilityDatabaseHelper.m
@@ -13,7 +13,7 @@
 // permissions and limitations under the License.
 //
 
-#import <AWSCore/AWSFMDB.h>
+#import "AWSFMDB.h"
 #import "AWSS3TransferUtilityDatabaseHelper.h"
 #import "AWSS3TransferUtility.h"
 #import "AWSS3TransferUtilityTasks.h"
